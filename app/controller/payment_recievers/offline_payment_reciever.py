63b90538bbc645b124c39d25c4723843d034ae4f
i~am~mim <mehedi.mim.bd@gmail.com>
2025-02-01 15:37:08 +0600

    Done" git push o

--- app/controller/payment_recievers/offline_payment_reciever.py
+++ app/controller/payment_recievers/offline_payment_reciever.py
@@ -5,7 +5,7 @@ from app.scalars.payment_recieved_scalars import ReceivedPayment
 payment_receiver_router = r = APIRouter()
 
 
-@r.post("offline_payment")
+@r.post("/offline_payment")
 def received_payment(data: ReceivedPayment, db = Depends(get_db)):
     data = "mim"
     return "Success"
